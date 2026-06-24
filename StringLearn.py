e409dd64d6575ff2d77804eaac66065d4b535ff1
paulsoumya07 <soumyarup1992@gmail.com>
2026-06-24 20:07:10 +0530

    String learn part 1 push

---
+++ StringLearn.py
@@ -0,0 +1,17 @@
+str = "Hi I am Soumyarup Paul.\nMy age is 24";
+print(str);
+print(len(str));
+
+str2 = "WTF??"
+str3 = str +" "+ str2;
+print(str3)
+print(str2[0])
+
+#WAP to input user name and print the length of the string
+name = input("Enter Your Name: ")
+print("The length of the string is:",len(name))
+
+#WAP tp count the occurance of "$" in a string
+str = "Hi mhy $ is adjadl $skfskf"
+x = str.count("$")
+print("Occurance of $ is ",x)
